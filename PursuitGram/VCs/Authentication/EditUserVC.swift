893804a5bb5940612a42472ce09e2bff5ba7fcb4
Adam <adamjackson@pursuit.org>
2019-12-01 22:54:25 -0500

    User editing

--- PursuitGram/VCs/Authentication/EditUserVC.swift
+++ PursuitGram/VCs/Authentication/EditUserVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Photos
 class EditUserVC: UIViewController {
     var user: AppUser!
     
@@ -19,10 +19,27 @@ class EditUserVC: UIViewController {
     
     var imageURL: URL? = nil
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var presentImagePicker: UIButton!
     @IBOutlet weak var userNameTextField: UITextField!
-    
+    @IBAction func presentPicker(_ sender: UIButton) {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .notDetermined, .denied, .restricted:
+            PHPhotoLibrary.requestAuthorization({[weak self] status in
+                switch status {
+                case .authorized:
+                    self?.presentPhotoPickerController()
+                case .denied:
+                    //MARK: TODO - set up more intuitive UI interaction
+                    print("Denied photo library permissions")
+                default:
+                    //MARK: TODO - set up more intuitive UI interaction
+                    print("No usable status")
+                }
+            })
+        default:
+            presentPhotoPickerController()
+        }
+    }
     @IBAction func saveChanges(_ sender: UIButton) {
         guard let userName = userNameTextField.text, let imageURL = imageURL else {
                    //MARK: TODO - alert
@@ -62,7 +79,42 @@ class EditUserVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    private func presentPhotoPickerController() {
+        DispatchQueue.main.async{
+            let imagePickerViewController = UIImagePickerController()
+            imagePickerViewController.delegate = self
+            imagePickerViewController.sourceType = .photoLibrary
+            imagePickerViewController.allowsEditing = true
+            imagePickerViewController.mediaTypes = ["public.image"]
+            self.present(imagePickerViewController, animated: true, completion: nil)
+        }
+    }
     
  
     
+}
+extension EditUserVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            //MARK: TODO - handle couldn't get image :(
+            return
+        }
+        self.image = image
+        
+        guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+            //MARK: TODO - gracefully fail out without interrupting UX
+            return
+        }
+        
+        FirebaseStorageService.manager.storeUserInputImage(image: imageData, completion: { [weak self] (result) in
+            switch result{
+            case .success(let url):
+                self?.imageURL = url
+            case .failure(let error):
+                //MARK: TODO - defer image not save alert, try again later. maybe make VC "dirty" to allow user to move on in nav stack
+                print(error)
+            }
+        })
+        dismiss(animated: true, completion: nil)
+    }
 }
